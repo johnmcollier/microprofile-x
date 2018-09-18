28076cf04fee45e10510b5fa1e332de97320a9ae
John Collier <John.J.Collier@ibm.com>
2018-09-17 21:55:09 -0400

    Skip the tests when running maven build.

--- watch.sh
+++ watch.sh
@@ -2,6 +2,6 @@
 
 # TEST
 # watch the java files and continously deploy the service
-mvn -B package liberty:install-apps -DskipTests=true
+mvn -B package -DskipTests=true liberty:install-apps -DskipTests=true
 skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
+reflex -r "\.java$" -- bash -c 'mvn -B package -DskipTests=true liberty:install-apps -DskipTests=true && skaffold run -p dev'
